7e4642bf2f16af904f7718366e865e48f2c7c6e8
Dev Parwal <dev.r.wal@gmail.com>
2025-01-16 17:27:01 -0600

    Climb Command Function Skeletons Added

--- src/main/java/frc/robot/commands/ClimbCommand1.java
+++ src/main/java/frc/robot/commands/ClimbCommand1.java
@@ -1,5 +1,28 @@
 package frc.robot.commands;
 
-public class ClimbCommand1 {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Climb;
+
+public class ClimbCommand1 extends Command{
+   // public final Climb climb; 
     
+    public ClimbCommand1() {
+
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {}
+
+    @Override
+    public void end(boolean interrupted) {}
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+
 }
