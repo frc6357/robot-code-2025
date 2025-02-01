fcfdac4ca8b76820bf90beff105aeb53e004c62c
CNelson0619 <nugg3t0619@gmail.com>
2025-02-01 11:43:35 -0600

    Defined Motor IDs & Made Instant Commands

--- src/main/java/frc/robot/bindings/ClimbBinder.java
+++ src/main/java/frc/robot/bindings/ClimbBinder.java
@@ -4,10 +4,11 @@ import static frc.robot.Ports.OperatorPorts.*;
 
 import java.util.Optional;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.ClimbCommand1;
-import frc.robot.commands.ClimbCommandReturn;
 import frc.robot.subsystems.SK25Climb;
+import static frc.robot.Konstants.ClimbConstants.*;
+
 
 
 public class ClimbBinder implements CommandBinder {
@@ -21,11 +22,14 @@ public class ClimbBinder implements CommandBinder {
         lower = climbLowerButton.button;
     }
 
-    public void bindButtons() {
-        if (subsystem.isPresent()) {
+    public void bindButtons() 
+    {
+        if (subsystem.isPresent()) 
+        {
             SK25Climb subsys = subsystem.get();
-            raise.onTrue(new ClimbCommand1(subsys));
-            lower.onTrue(new ClimbCommandReturn(subsys));
+           // raise.onTrue(new ClimbCommand1(subsys));
+           // lower.onTrue(new ClimbCommandReturn(subsys));
+           raise.onTrue(new InstantCommand(() -> subsys.runMotor(kSpeed)));
         }
     }
 }
