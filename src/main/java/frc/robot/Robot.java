5769a2154a254ef3d3094ad31b12e28e59ac4e58
NickM792 <ds010927nm@dsisdconnect.com>
2025-02-07 21:25:16 -0600

    Big Additions: Elastic API and Controller Creation Handling
    Added the Elastic.java file which contains methods to interact with Elastic Dashboard.
    
    Also added SKController.java, which allows controller construction to be made simple by specifying the type and port in its constructor. It will handle the binding of buttons no matter the type to the same collection of methods (which create new SKTrigger objects).
    
    Lastly, added SKCommandXboxController which extends SKTrigger and makes new triggers for FilteredXboxController objects.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.net.WebServer;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -32,6 +34,9 @@ public class Robot extends TimedRobot
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
+
+        //get the saved elastic dashboard layout
+        WebServer.start(5800, Filesystem.getDeployDirectory().getPath());
     }
 
     /**
