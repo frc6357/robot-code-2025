a7d4fcf785af4a6e5efce6df76d0a86ca1800f81
scootar <gavinmacalpine@gmail.com>
2025-02-12 20:40:03 -0600

    RotationalController documenting

--- src/main/java/frc/robot/subsystems/swerve/RotationController.java
+++ src/main/java/frc/robot/subsystems/swerve/RotationController.java
@@ -34,7 +34,7 @@ public class RotationController {
 
         // Effectively creates a unit circle of input for cocentric angles
         controller.enableContinuousInput(-Math.PI, Math.PI); 
-        
+
         // Sets the tolerance for the difference between robot and goal angle
         // It is relatively looser than the HoldController tolerance to allow for smoother movement
         controller.setTolerance(config.getRotationTolerance()); 
@@ -48,29 +48,48 @@ public class RotationController {
 
         holdController.enableContinuousInput(-Math.PI, Math.PI);
         holdController.setTolerance(
-                config.getRotationTolerance() / 2); // Half the tolerance of turn controller
+                config.getRotationTolerance() / 2); // Half the error tolerance of turn controller
     }
 
+    /**
+     * Calculates the power needed to rotate the robot to a set point based on which type
+     * of PID controller is used (Rotating vs. Holding Controller)
+     * 
+     * @param goalRadians      The goal rotation of the robot in radians
+     * @param currentRadians   The current rotation of the robot in radians
+     * @param isHoldController Whether or not the controller should use the hold controller PID tuning
+     * @return The controller output
+     */
     public double calculate(double goalRadians, double currentRadians, boolean isHoldController) {
         double measurement = currentRadians;
-        calculatedValue = controller.calculate(measurement, goalRadians);
+        calculatedValue = controller.calculate(measurement, goalRadians); // Based on whether or not hold controller, calculate motor power needed
 
         if (atSetpoint()) {
-            if (isHoldController) {
+            if (isHoldController) { // If hold controller, use the hold controller's aggressive tuning
                 calculatedValue = calculateHold(goalRadians, currentRadians);
                 return calculatedValue;
             }
-            calculatedValue = 0;
+            calculatedValue = 0; // (Basically) Else: no movement necesarry for rotating controller because robot has reached goal
             return calculatedValue;
         } else {
             return calculatedValue;
         }
     }
 
+    /**
+     * Same function as {@link RotationController#calculate(double, double, boolean)},
+     * but assumes the desired PID controller is the rotating controller. 
+     */
     public double calculate(double goalRadians, double currentRadians) {
         return calculate(goalRadians, currentRadians, false);
     }
 
+    /**
+     * Calculates the power needed for the Hold controller to maintain the robot's rotation
+     * @param goalRadians       The goal rotation of the robot in radians
+     * @param currentRadians    The current rotation of the robot in radians
+     * @return The hold controller's output
+     */
     public double calculateHold(double goalRadians, double currentRadians) {
         calculatedValue = holdController.calculate(currentRadians, goalRadians);
         return calculatedValue;
@@ -80,11 +99,21 @@ public class RotationController {
         return controller.atSetpoint();
     }
 
+    /**
+     * Zeroes both rotational controllers, feeding the current rotation as the zero point.
+     * @param currentRadians    The current rotation of the robot in radians
+     */
     public void reset(double currentRadians) {
         controller.reset(currentRadians);
         holdController.reset();
     }
 
+    /**
+     * Updates the PID values of the rotating controller; the hold controller does not change.
+     * @param kP    The proportional coefficient. Must be >= 0.
+     * @param kI    The integral coefficient. Must be >= 0.
+     * @param kD    The differential coefficient. Must be >= 0.
+     */
     public void updatePID(double kP, double kI, double kD) {
         controller.setPID(kP, kI, kD);
     }
