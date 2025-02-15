0c2730fc9d80075f13ca21aa06ce349666e5631c
ERobin07 <156146464+ERobin07@users.noreply.github.com>
2025-02-15 17:53:28 -0600

    Other PID Stuff

--- src/main/java/frc/robot/subsystems/SK25Elevator.java
+++ src/main/java/frc/robot/subsystems/SK25Elevator.java
@@ -16,11 +16,11 @@ import static frc.robot.Ports.ElevatorPorts.kRightElevatorMotor;
 
 // Encoder V3 (Still REV)
 import com.revrobotics.RelativeEncoder;
-//import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.ClosedLoopSlot;
 
 // Motors - Sparkflex
 import com.revrobotics.spark.SparkBase;
-//import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -98,19 +98,19 @@ public class SK25Elevator extends Elevator
             .positionConversionFactor(1)
             .velocityConversionFactor(1);
         motorConfigL.closedLoop
-            .feedbackSensor(FeedbackSensor.kPrimaryEncoder);
-            //.velocityFF(1.0 / 5767, ClosedLoopSlot.kSlot1);
-            // Set PID values for position control. We don't need to pass a closed loop
-            // slot, as it will default to slot 0.
-            //.p(0.1)
-            //.i(0)
-            //.d(0)
-            //.outputRange(-1, 1)
-            // Set PID values for velocity control in slot 1
-            //.p(0.0001, ClosedLoopSlot.kSlot1)
-            //.i(0, ClosedLoopSlot.kSlot1)
-            //.d(0, ClosedLoopSlot.kSlot1)
-            //.outputRange(-1, 1, ClosedLoopSlot.kSlot1);
+            .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+            .velocityFF(1.0 / 565, ClosedLoopSlot.kSlot1)
+            //Set PID values for position control. We don't need to pass a closed loop
+            //slot, as it will default to slot 0.
+            .p(0.1)
+            .i(0)
+            .d(0)
+            .outputRange(-1, 1)
+            //Set PID values for velocity control in slot 1
+            .p(0.0001, ClosedLoopSlot.kSlot1)
+            .i(0, ClosedLoopSlot.kSlot1)
+            .d(0, ClosedLoopSlot.kSlot1)
+            .outputRange(-1, 1, ClosedLoopSlot.kSlot1);
 
         // Configurations For The Right Motor
         motorConfigR
