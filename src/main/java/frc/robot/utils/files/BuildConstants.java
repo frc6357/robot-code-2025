80813bb7ab243c4dba8f075a0da0d446666b96ec
NickM792 <ds010927nm@dsisdconnect.com>
2025-03-12 18:14:04 -0500

    removed buildconstants and changed file directory

--- src/main/java/frc/robot/utils/files/BuildConstants.java
+++ src/main/java/frc/robot/utils/files/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 276;
-  public static final String GIT_SHA = "32528c368de4a3823e2af7f0cdc31c9663d20835";
-  public static final String GIT_DATE = "2025-03-10 09:57:01 EDT";
+  public static final int GIT_REVISION = 290;
+  public static final String GIT_SHA = "d2e87ec7fc3d65e03614cae12419b5e5fa74404a";
+  public static final String GIT_DATE = "2025-03-11 20:17:04 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-10 11:06:08 EDT";
-  public static final long BUILD_UNIX_TIME = 1741619168695L;
+  public static final String BUILD_DATE = "2025-03-12 19:13:29 EDT";
+  public static final long BUILD_UNIX_TIME = 1741821209511L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
