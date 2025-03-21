066229ecb5273d9cec9e3e5546e520ddcbd32918
ERobin07 <156146464+ERobin07@users.noreply.github.com>
2025-03-21 12:44:40 -0500

    Station auto changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "robot-code-2025";
+  public static final String MAVEN_NAME = "frc6357-robot_code_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 282;
-  public static final String GIT_SHA = "44be803e4b7cc25da20f99fb001f5eefb7ac0d06";
-  public static final String GIT_DATE = "2025-03-21 12:16:03 EDT";
+  public static final int GIT_REVISION = 283;
+  public static final String GIT_SHA = "2e1c08a3ad717d1c4685dfcc7e4bf00d74ccc424";
+  public static final String GIT_DATE = "2025-03-21 13:10:00 EDT";
   public static final String GIT_BRANCH = "LidarRevTest";
-  public static final String BUILD_DATE = "2025-03-21 12:17:05 EDT";
-  public static final long BUILD_UNIX_TIME = 1742573825964L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-21 13:17:58 EDT";
+  public static final long BUILD_UNIX_TIME = 1742577478965L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
