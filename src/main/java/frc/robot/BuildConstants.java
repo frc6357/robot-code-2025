d212c3c84fbdeb03c90d420dc12129ea79822280
ERobin07 <156146464+ERobin07@users.noreply.github.com>
2025-03-24 19:12:55 -0500

    testing ProcessorSideCoral

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc6357-robot_code_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 313;
-  public static final String GIT_SHA = "6062e085eb463842d07f38b1cfd29134b51d2dc0";
-  public static final String GIT_DATE = "2025-03-24 17:28:41 EDT";
+  public static final int GIT_REVISION = 316;
+  public static final String GIT_SHA = "5df7293ddad7ff303a0cf208ef3440e7c8b5de8b";
+  public static final String GIT_DATE = "2025-03-24 19:33:16 EDT";
   public static final String GIT_BRANCH = "revAutoCommandTesting";
-  public static final String BUILD_DATE = "2025-03-24 18:42:07 EDT";
-  public static final long BUILD_UNIX_TIME = 1742856127888L;
+  public static final String BUILD_DATE = "2025-03-24 20:10:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1742861447403L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
