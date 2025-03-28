54013ec4c0521347267fe8e42547379e5afc32d8
scootar <gavinmacalpine@gmail.com>
2025-03-27 22:19:21 -0500

    worked first try omg

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 329;
-  public static final String GIT_SHA = "c0d95f6de70d67dad8551dabce3d81ce77b7ada0";
-  public static final String GIT_DATE = "2025-03-27 15:42:01 EDT";
-  public static final String GIT_BRANCH = "Competition/SpaceCity";
-  public static final String BUILD_DATE = "2025-03-27 18:18:09 EDT";
-  public static final long BUILD_UNIX_TIME = 1743113889342L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 374;
+  public static final String GIT_SHA = "97f9c5ca337976204a9dff038b03cf80142cee68";
+  public static final String GIT_DATE = "2025-03-27 23:07:44 EDT";
+  public static final String GIT_BRANCH = "SpaceCityVisionDevelop";
+  public static final String BUILD_DATE = "2025-03-27 23:15:22 EDT";
+  public static final long BUILD_UNIX_TIME = 1743131722365L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
