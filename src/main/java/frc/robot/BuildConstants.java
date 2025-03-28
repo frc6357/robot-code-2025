4ca76fa9c0f2e71aa50b3a63bbd997b4935bef29
scootar <gavinmacalpine@gmail.com>
2025-03-28 18:37:23 -0500

    updated venderops

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "robot-code-2025";
+  public static final String MAVEN_NAME = "frc6357-robot_code_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 329;
-  public static final String GIT_SHA = "c0d95f6de70d67dad8551dabce3d81ce77b7ada0";
-  public static final String GIT_DATE = "2025-03-27 15:42:01 EDT";
+  public static final int GIT_REVISION = 333;
+  public static final String GIT_SHA = "1a1b01340e5a87e9557edf086d29e20855145937";
+  public static final String GIT_DATE = "2025-03-27 19:17:11 EDT";
   public static final String GIT_BRANCH = "Competition/SpaceCity";
-  public static final String BUILD_DATE = "2025-03-27 18:18:09 EDT";
-  public static final long BUILD_UNIX_TIME = 1743113889342L;
+  public static final String BUILD_DATE = "2025-03-28 19:00:40 EDT";
+  public static final long BUILD_UNIX_TIME = 1743202840866L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
