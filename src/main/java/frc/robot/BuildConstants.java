3e94337b6e62b38dd2e04feda97b17a338ebc3af
scootar <gavinmacalpine@gmail.com>
2025-03-29 14:00:28 -0500

    headon changes auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc6357-robot_code_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 339;
-  public static final String GIT_SHA = "a6fb8551007856a54fbe7de87dd5ee4077b2b7a1";
-  public static final String GIT_DATE = "2025-03-29 12:05:56 EDT";
+  public static final int GIT_REVISION = 340;
+  public static final String GIT_SHA = "8831fe5711bca1d323d4abcc472bc708ca9eeea8";
+  public static final String GIT_DATE = "2025-03-29 14:29:30 EDT";
   public static final String GIT_BRANCH = "Competition/SpaceCity";
-  public static final String BUILD_DATE = "2025-03-29 14:24:01 EDT";
-  public static final long BUILD_UNIX_TIME = 1743272641967L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-29 14:43:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1743273826184L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
