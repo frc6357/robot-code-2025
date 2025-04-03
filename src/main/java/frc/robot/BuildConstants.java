93e2a81ff36fea3a30d0786fdd7f51add7790727
scootar <gavinmacalpine@gmail.com>
2025-04-03 17:12:12 -0500

    auto chnages after match 3

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 358;
-  public static final String GIT_SHA = "9eb63ef391a8d18ed92eb2842f77b822a071d168";
-  public static final String GIT_DATE = "2025-04-02 11:16:01 EDT";
+  public static final int GIT_REVISION = 359;
+  public static final String GIT_SHA = "1aa958d059ef67622eda89e686141466a1687691";
+  public static final String GIT_DATE = "2025-04-03 15:09:03 EDT";
   public static final String GIT_BRANCH = "compeition/State";
-  public static final String BUILD_DATE = "2025-04-03 14:36:40 EDT";
-  public static final long BUILD_UNIX_TIME = 1743705400574L;
+  public static final String BUILD_DATE = "2025-04-03 16:52:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1743713567364L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -103,7 +103,7 @@ public class CoralSubsystem extends SubsystemBase {
     final Pref<Double> elevatorVelocity = SKPreferences.attach("elevatorVelocity", 4000.0) //2500.0
       .onChange((unused) -> reconfigureElevator());
 
-    final Pref<Double> elevatorAcceleration = SKPreferences.attach("elevatorAcceleration", 3000.0) //6000
+    final Pref<Double> elevatorAcceleration = SKPreferences.attach("elevatorAcceleration", 3500.0) //6000
     .onChange((newValue) -> reconfigureElevator());
   
     private void reconfigureElevator() {
