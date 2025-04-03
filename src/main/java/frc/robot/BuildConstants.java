5a45ceeec908e31ba7b0508c39fee967490816f8
scootar <gavinmacalpine@gmail.com>
2025-04-03 11:18:52 -0500

    vision auto adjustments!

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 418;
-  public static final String GIT_SHA = "3264cee17522262fef39eacd40120872a723b283";
-  public static final String GIT_DATE = "2025-04-03 10:02:11 EDT";
+  public static final int GIT_REVISION = 419;
+  public static final String GIT_SHA = "744c32c2cfa86c53a679b875c992b189fc37f639";
+  public static final String GIT_DATE = "2025-04-03 11:51:53 EDT";
   public static final String GIT_BRANCH = "SpaceCityVisionDevelop";
-  public static final String BUILD_DATE = "2025-04-03 11:10:10 EDT";
-  public static final long BUILD_UNIX_TIME = 1743693010936L;
+  public static final String BUILD_DATE = "2025-04-03 12:03:25 EDT";
+  public static final long BUILD_UNIX_TIME = 1743696205750L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
