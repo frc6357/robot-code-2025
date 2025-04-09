559282cd8fde49454fb02c929182aec239120313
ERobin07 <156146464+ERobin07@users.noreply.github.com>
2025-04-09 15:27:18 -0500

    new wheel radius, pp dimensions, and TunerConstants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc6357-robot_code_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 386;
-  public static final String GIT_SHA = "c6f43998a487f891b9927089ad9bf3c17e03c847";
-  public static final String GIT_DATE = "2025-04-09 10:16:46 EDT";
+  public static final int GIT_REVISION = 387;
+  public static final String GIT_SHA = "87dbe58df5d7c6e8e53aa0b82c2ce0e82cc5c1f7";
+  public static final String GIT_DATE = "2025-04-09 11:30:22 EDT";
   public static final String GIT_BRANCH = "compeition/State";
-  public static final String BUILD_DATE = "2025-04-09 11:08:24 EDT";
-  public static final long BUILD_UNIX_TIME = 1744211304926L;
+  public static final String BUILD_DATE = "2025-04-09 16:03:17 EDT";
+  public static final long BUILD_UNIX_TIME = 1744228997315L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/TunerConstants.java
+++ src/main/java/frc/robot/TunerConstants.java
@@ -114,7 +114,7 @@ public class TunerConstants {
 
     private static final double kDriveGearRatio = 6.746031746031747;
     private static final double kSteerGearRatio = 21.428571428571427;
-    private static final Distance kWheelRadius = Inches.of(1.9); //2    //1.9
+    private static final Distance kWheelRadius = Inches.of(1.9094); //2    //1.9
 
     private static final boolean kInvertLeftSide = false; //false
     private static final boolean kInvertRightSide = true;  //true
@@ -161,7 +161,7 @@ public class TunerConstants {
     private static final int kFrontLeftDriveMotorId = 1;
     private static final int kFrontLeftSteerMotorId = 11;
     private static final int kFrontLeftEncoderId = 21;
-    private static final Angle kFrontLeftEncoderOffset = Rotations.of(-0.284423828125);
+    private static final Angle kFrontLeftEncoderOffset = Rotations.of(-0.28857421875); //-0.284423828125
     private static final boolean kFrontLeftSteerMotorInverted = true;
     private static final boolean kFrontLeftEncoderInverted = false;
 
@@ -172,7 +172,7 @@ public class TunerConstants {
     private static final int kFrontRightDriveMotorId = 2;
     private static final int kFrontRightSteerMotorId = 12;
     private static final int kFrontRightEncoderId = 22;
-    private static final Angle kFrontRightEncoderOffset = Rotations.of(-0.289794921875);
+    private static final Angle kFrontRightEncoderOffset = Rotations.of(-0.290283203125); //-0.289794921875
     private static final boolean kFrontRightSteerMotorInverted = true;
     private static final boolean kFrontRightEncoderInverted = false;
 
@@ -182,7 +182,7 @@ public class TunerConstants {
     private static final int kBackLeftDriveMotorId = 3;
     private static final int kBackLeftSteerMotorId = 13;
     private static final int kBackLeftEncoderId = 23;
-    private static final Angle kBackLeftEncoderOffset = Rotations.of(0.1337890625);
+    private static final Angle kBackLeftEncoderOffset = Rotations.of(0.132080078125); //0.1337890625
     private static final boolean kBackLeftSteerMotorInverted = true;
     private static final boolean kBackLeftEncoderInverted = false;
 
@@ -193,7 +193,7 @@ public class TunerConstants {
     private static final int kBackRightDriveMotorId = 4;
     private static final int kBackRightSteerMotorId = 14;
     private static final int kBackRightEncoderId = 24;
-    private static final Angle kBackRightEncoderOffset = Rotations.of(-0.100341796875);
+    private static final Angle kBackRightEncoderOffset = Rotations.of(-0.09765625); //-0.100341796875
     private static final boolean kBackRightSteerMotorInverted = true;
     private static final boolean kBackRightEncoderInverted = false;
 
