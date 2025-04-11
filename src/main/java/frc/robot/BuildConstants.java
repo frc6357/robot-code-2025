c18a93771816d49aca9c1b382a2e5ee2c3800329
scootar <gavinmacalpine@gmail.com>
2025-04-11 15:48:12 -0500

    working 2gp barge side

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "frc6357-robot_code_2025";
+  public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 469;
-  public static final String GIT_SHA = "bff20786bf1971bc4bea301179db1ff4145c8bd3";
-  public static final String GIT_DATE = "2025-04-10 19:29:22 EDT";
+  public static final int GIT_REVISION = 470;
+  public static final String GIT_SHA = "38df4b24ebe91a7a4fb86e221bf7cdd23b60d5a3";
+  public static final String GIT_DATE = "2025-04-10 19:59:48 EDT";
   public static final String GIT_BRANCH = "OldVisionTest";
-  public static final String BUILD_DATE = "2025-04-10 19:56:04 EDT";
-  public static final long BUILD_UNIX_TIME = 1744329364027L;
+  public static final String BUILD_DATE = "2025-04-11 16:46:28 EDT";
+  public static final long BUILD_UNIX_TIME = 1744404388550L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
