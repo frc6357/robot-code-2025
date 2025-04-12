e09ba01d05bb6af84a5cf30273a4694ad30c7a38
scootar <gavinmacalpine@gmail.com>
2025-04-12 16:56:29 -0500

    processor side auto adjustments

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "frc6357-robot_code_2025";
+  public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 475;
-  public static final String GIT_SHA = "971af3f5cc35fda5f738b8ccf1ee0000633bbf6a";
-  public static final String GIT_DATE = "2025-04-12 17:13:32 EDT";
+  public static final int GIT_REVISION = 476;
+  public static final String GIT_SHA = "5fce587541d2b0e418b030c641691d022bd00bb6";
+  public static final String GIT_DATE = "2025-04-12 17:36:44 EDT";
   public static final String GIT_BRANCH = "VisionMain";
-  public static final String BUILD_DATE = "2025-04-12 17:33:13 EDT";
-  public static final long BUILD_UNIX_TIME = 1744493593112L;
+  public static final String BUILD_DATE = "2025-04-12 17:51:15 EDT";
+  public static final long BUILD_UNIX_TIME = 1744494675019L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
