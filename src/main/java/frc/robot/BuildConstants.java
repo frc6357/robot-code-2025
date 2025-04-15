e2b6721d9665ce3239143e3eb0947fcf67178185
scootar <gavinmacalpine@gmail.com>
2025-04-15 14:02:05 -0500

    three net auto scores but needs optimizations

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc6357-robot_code_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 493;
-  public static final String GIT_SHA = "0711031a9ce1c8cd7d2583802df5c170b8752741";
-  public static final String GIT_DATE = "2025-04-15 13:31:20 EDT";
+  public static final int GIT_REVISION = 494;
+  public static final String GIT_SHA = "b1171ed4e9e2e31894d00c9077c4eaedead0f610";
+  public static final String GIT_DATE = "2025-04-15 13:51:43 EDT";
   public static final String GIT_BRANCH = "VisionMain";
-  public static final String BUILD_DATE = "2025-04-15 13:40:47 EDT";
-  public static final long BUILD_UNIX_TIME = 1744738847420L;
+  public static final String BUILD_DATE = "2025-04-15 15:00:00 EDT";
+  public static final long BUILD_UNIX_TIME = 1744743600756L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
