bec3e84f2b7df18b42317b675dad44c10020c4df
ERobin07 <156146464+ERobin07@users.noreply.github.com>
2025-04-16 18:38:56 -0500

    added race groups with timeouts for auto align commands in autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc6357-robot_code_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 497;
-  public static final String GIT_SHA = "6bba099ca4d588dba75f4fcb24ddef9a12b0c532";
-  public static final String GIT_DATE = "2025-04-15 19:51:06 EDT";
-  public static final String GIT_BRANCH = "autoClimbTestAtWorlds";
-  public static final String BUILD_DATE = "2025-04-16 17:35:14 EDT";
-  public static final long BUILD_UNIX_TIME = 1744839314382L;
+  public static final int GIT_REVISION = 499;
+  public static final String GIT_SHA = "33fd177b76ac007a7db2db8e9144a594e8cf75eb";
+  public static final String GIT_DATE = "2025-04-16 17:40:04 EDT";
+  public static final String GIT_BRANCH = "Competition/Worlds";
+  public static final String BUILD_DATE = "2025-04-16 19:36:51 EDT";
+  public static final long BUILD_UNIX_TIME = 1744846611539L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
