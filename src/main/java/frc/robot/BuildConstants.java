91d98b2c0a3e83c6374380ef7d5bdbbed3fb161a
ERobin07 <156146464+ERobin07@users.noreply.github.com>
2025-04-17 12:06:09 -0500

    adjusted autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc6357-robot_code_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 500;
-  public static final String GIT_SHA = "bec3e84f2b7df18b42317b675dad44c10020c4df";
-  public static final String GIT_DATE = "2025-04-16 19:38:56 EDT";
+  public static final int GIT_REVISION = 501;
+  public static final String GIT_SHA = "d6c2d6b3de2e0622394c87e418a0b39d9cb5e552";
+  public static final String GIT_DATE = "2025-04-17 11:18:39 EDT";
   public static final String GIT_BRANCH = "Competition/Worlds";
-  public static final String BUILD_DATE = "2025-04-17 11:18:15 EDT";
-  public static final long BUILD_UNIX_TIME = 1744903095509L;
+  public static final String BUILD_DATE = "2025-04-17 12:39:33 EDT";
+  public static final long BUILD_UNIX_TIME = 1744907973017L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
