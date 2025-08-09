e491f9e5b56983d68b90d7cb44104df1a1935031
scootar <gavinmacalpine@gmail.com>
2025-08-09 08:17:20 -0500

    Fixed HotswapBinder; fully functioning

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 510;
-  public static final String GIT_SHA = "0bb11d673ab933f477f674b7d3e0c84a79456dae";
-  public static final String GIT_DATE = "2025-06-28 14:11:00 EDT";
-  public static final String GIT_BRANCH = "Competition/Worlds";
-  public static final String BUILD_DATE = "2025-06-28 15:14:50 EDT";
-  public static final long BUILD_UNIX_TIME = 1751138090517L;
+  public static final int GIT_REVISION = 512;
+  public static final String GIT_SHA = "0b368a8e5c9ae13d614263aa89900b140961f3a7";
+  public static final String GIT_DATE = "2025-07-25 14:11:40 EDT";
+  public static final String GIT_BRANCH = "OneControllerSysCheck";
+  public static final String BUILD_DATE = "2025-08-09 09:14:52 EDT";
+  public static final long BUILD_UNIX_TIME = 1754745292145L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
