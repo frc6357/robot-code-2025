0c8039663b2dc491e18e07f5d08edb1ad9df631c
scootar <gavinmacalpine@gmail.com>
2025-08-09 13:05:14 -0500

    buildconstants update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 513;
-  public static final String GIT_SHA = "255f008e5f332a1d383fa4c6bb50d4b0def6363d";
-  public static final String GIT_DATE = "2025-08-06 19:51:04 EDT";
-  public static final String GIT_BRANCH = "Competition/Worlds";
-  public static final String BUILD_DATE = "2025-08-09 09:23:04 EDT";
-  public static final long BUILD_UNIX_TIME = 1754745784773L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 517;
+  public static final String GIT_SHA = "46a32bdea1595c0a528eff94c4b7754f9b5c1913";
+  public static final String GIT_DATE = "2025-08-09 09:41:12 EDT";
+  public static final String GIT_BRANCH = "HotswapMerge";
+  public static final String BUILD_DATE = "2025-08-09 09:42:00 EDT";
+  public static final long BUILD_UNIX_TIME = 1754746920643L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
