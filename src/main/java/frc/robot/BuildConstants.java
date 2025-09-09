13653e3e06eee808f3d087828e9b69c9d8ac4c53
scootar <gavinmacalpine@gmail.com>
2025-09-09 18:30:37 -0500

    PathPlanner progress

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,21 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
+<<<<<<< Updated upstream
   public static final int GIT_REVISION = 517;
   public static final String GIT_SHA = "46a32bdea1595c0a528eff94c4b7754f9b5c1913";
   public static final String GIT_DATE = "2025-08-09 09:41:12 EDT";
   public static final String GIT_BRANCH = "HotswapMerge";
   public static final String BUILD_DATE = "2025-08-09 09:42:00 EDT";
   public static final long BUILD_UNIX_TIME = 1754746920643L;
+=======
+  public static final int GIT_REVISION = 522;
+  public static final String GIT_SHA = "bf574441a2180ca7a943da7f8cd14ed5a153e955";
+  public static final String GIT_DATE = "2025-08-26 16:58:14 EDT";
+  public static final String GIT_BRANCH = "dev/FollowVisionTarget";
+  public static final String BUILD_DATE = "2025-08-26 19:16:25 EDT";
+  public static final long BUILD_UNIX_TIME = 1756250185758L;
+>>>>>>> Stashed changes
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
